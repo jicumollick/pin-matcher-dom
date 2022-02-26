858890841988b1aa9721cfa38918937714568d37
jicu mollick <jikumollick82@gmail.com>
2022-02-26 23:50:25 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+
+
+function getPin(){
+    const pin = Math.round(Math.random() * 10000);
+
+    let pinString = pin + '';
+    if(pinString.length == 4){
+        return pin;
+
+    }else {
+        getPin();
+    }
+
+}
+
+
+function generatePin(){
+
+   let pin = getPin();
+
+   document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+
+    const number = event.target.innerText;
+
+    const calc = document.getElementById('typed-numbers');
+
+
+    if(isNaN(number)){
+        if(number == 'C'){
+            calc.value = '';
+        }
+
+    }else {
+        const previousCalc    = calc.value; 
+   
+        const newCalc = previousCalc + number;
+        calc.value = newCalc;
+    }
+} )
+
+function veryfyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers  = document.getElementById('typed-numbers').value;
+    
+    if(pin == typedNumbers){
+        document.getElementById('notify-sucess').style.display = "block";
+        document.getElementById('notify-fail').style.display = "none";
+
+    }else {
+        document.getElementById('notify-fail').style.display = "block";
+        document.getElementById('notify-sucess').style.display = "none";
+
+    }
+}
